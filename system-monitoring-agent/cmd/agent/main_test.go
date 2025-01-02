72f819c231298683b9efe32b35e9a2825a01abc7
travis martin <travismartin266@gmail.com>
2025-01-02 07:49:27 -0400

    Trying to add more tests

---
+++ system-monitoring-agent/cmd/agent/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// TODO: Add test cases for main function
+	// This will likely require refactoring main() to be more testable
+	// Consider moving core logic to separate functions that can be tested independently
+}
